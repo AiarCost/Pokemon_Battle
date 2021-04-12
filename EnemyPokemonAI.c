a7c6c629bbd5e8dacc2209c888267c63bddb5c54
AiarCost <Ariana.A80@outlook.com>
2021-04-11 18:05:37 -0700

    Enemy AI can choose attacks

--- EnemyPokemonAI.c
+++ EnemyPokemonAI.c
@@ -1,3 +1,13 @@
+/***************************************************
+
+Contributors: Ariana Acosta, Katelyn Ripa
+
+Summary: Enemy AI and where the enemy chooses what it should do
+
+
+**************************************************/
+
+
 #include <stdio.h>
 #include "GeneratePokemon.c"
 #include <stdlib.h>
@@ -8,61 +18,49 @@ void EnemyAIChoose(Pokemon Enemy, int enemyMaxHP);
 int main(){
 
   Pokemon enemy = GenerateStats(-1);
-  int enemyMaxHP = enemy.HP;
-  EnemyAIChoose(enemy);
-  
-
+  int enemyMaxHP = enemy.pokeHP;
+  EnemyAIChoose(enemy, enemyMaxHP);
 return 0;
 }
 
 
 void EnemyAIChoose(Pokemon Enemy, int enemyMaxHP){
 
-  float hpPercentage = Enemy.HP/enemyMaxHP;
+  float hpPercentage = Enemy.pokeHP/enemyMaxHP;
   srand(time(NULL));
-  float RandomRoll = rand()%100+1
+  float RandomRoll = rand()%100+1;
       
-  
   if( hpPercentage <= .5f){
       //.5% = 35-29.5 .01% = 35-.5
-      if(RandomRoll <= (35 - ((hpPercentage*100)/1.7f)){
+      if(RandomRoll <= (35 - ((hpPercentage*100)/1.7f))){
           //Enemy pokemon flees
           printf("Enemy fleeing!\n");
           }
-          
       else{
-      
-        RandomRoll=RandomRoll%2+1
+          RandomRoll=(int)(RandomRoll+.5)%2+1;
         
-        if(RandomRoll == 1){
-          //Enemy Attack 1
-          printf("Enemy Attack 1\n");
-        }
-        else{
-          //Enemy Attack 2
-          printf("Enemy Attack 2\n");
-        
+          if(RandomRoll == 1){
+            //Enemy Attack 1
+            printf("Enemy Attack 1\n");
+          }
+          else{
+            //Enemy Attack 2
+            printf("Enemy Attack 2\n");
         }
       }
-      
-          
-      else{
-      
-        RandomRoll=RandomRoll%2+1
+  }   
+  else{
+    RandomRoll=(int)(RandomRoll+.5)%2+1;
         
-        if(RandomRoll == 1){
-          //Enemy Attack 1
-          printf("Enemy Attack 1\n");
-        }
-        else{
-          //Enemy Attack 2
-          printf("Enemy Attack 2\n");
-        
-        }
+    if(RandomRoll == 1){
+      //Enemy Attack 1
+      printf("Enemy Attack 1\n");
+    }
+    else{
+      //Enemy Attack 2
+      printf("Enemy Attack 2\n");
+    }
       
-      }
   }
-  
-  
-
 }
+
